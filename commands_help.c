3ff980f0d50d78ffb5dfbf08b603a2ca20166098
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-22 20:48:34 +0100

    sd

---
+++ commands_help.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commands_help.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 17:51:54 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/12/22 18:35:25 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_link *create_cpy(char *str)
+{
+    int j;
+    char *content;
+    t_link *new;
+    int i;
+
+    j = 0;
+    while (str[j])
+        j++;
+    new = malloc(sizeof(t_link));
+    if (!new)
+        return (NULL);
+    content = malloc(j + 1);
+    if (!content)
+        return (free(new),NULL);
+    i = 0;
+    while (str[i])
+    {
+        content[i] = str[i];
+        i++;
+    }
+    content[i]='\0';
+    new->content = content;
+    return (new);
+}
+
+void    *check_int(char *argv)
+{
+    int j;
+
+    j = 0;
+    while (argv[j]) 
+    {
+        if (argv[j] < '0' || argv[j] > '9')
+            return (NULL);
+        j++;
+    }
+    return (argv);
+}
+
+void    change_link(t_link **stack,t_link **new)
+{
+    if (*stack == NULL)
+    {
+        *stack = *new;
+        (*new)->next = *new;
+        (*new)->prev = *new;
+    }
+    else 
+    {
+        (*new)->next = *stack;
+        (*new)->prev = (*stack)->prev; 
+        (*stack)->prev->next = *new;
+        (*stack)->prev = *new;
+        *stack = *new;
+    }
+}
+
