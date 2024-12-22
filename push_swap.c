bcabc1f44492caa4af5e78c80642f54e81166aec
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-22 14:13:38 +0100

    df

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:16:21 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/20 16:39:12 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/22 14:12:39 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,57 +36,71 @@ char *cpy(char *str)
     return (ptr);
 }
 
+void swap(t_link **first,t_link **second)
+{
+    void *cup;
+    
+    cup = (*first)->content;
+    (*first)->content = (*second)->content;
+    (*second)->content = cup;
+}
+
 
 int main()
 {
+    
+// -------------------------------------------------
     t_link *node;
     t_link *node2;
     t_link *node3;
-    
+
+    int i = 1;
+    int i2 = 2;
+    int i3 = 3;
+
+
     node = malloc(sizeof(t_link));
-    node->content = cpy("hello");
+    node->content = &i;
     
     node2 = malloc(sizeof(t_link));
-    node2->content = cpy("hello2");
+    node2->content = &i2;
 
     node3 = malloc(sizeof(t_link));
-    node3->content = cpy("hello3");
+    node3->content = &i3;
 
 
     node->next = node2;
     node2->next = node3;
     node3->next = node;
-;
+
     node->prev = node3;
     node2->prev = node;
     node3->prev = node2;
 
+// -------------------------------------------------
+    t_link *list;
+
+    list = malloc(sizeof(t_link));
+
+// -------------------------------------------------
+
+    swap(&node,&node2);
+
+
+
+// -------------------------------------------------
 
     printf("node 1\n");
-    printf("%s\n", (char*)node->content);
-
-    printf("%s\n",(char*)node->next->content);
-
-    printf("%s\n",(char*)node->prev->content);
-
-    printf("node 2\n");    
-    printf("%s\n", (char*)node2->content);
-
-    printf("%s\n",(char*)node2->next->content);
-
-    printf("%s\n",(char*)node2->prev->content);
+    printf("%d\n",*(int *)node->content);
     
+    printf("node 2\n");
+    printf("%d\n",*(int *)node2->content);
+
     printf("node 3\n");
-    printf("%s\n", (char*)node3->content);
+    printf("%d\n",*(int *)node3->content);
 
-    printf("%s\n",(char*)node3->next->content);
 
-    printf("%s\n",(char*)node3->prev->content);
-
-    free(node->content);
     free(node);
-    free(node2->content);
     free(node2);
-    free(node3->content);
     free(node3);
 }
