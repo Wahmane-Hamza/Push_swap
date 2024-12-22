880d0ed210637b0d8f7e1c69925baea8f1281e40
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-22 16:06:42 +0100

    df

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:16:21 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/22 14:12:39 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/22 16:04:08 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ char *cpy(char *str)
     j = 0;
     while (str[j])
         j++;
-
     ptr = malloc(j + 1);
     if (!ptr)
         return (NULL);
@@ -45,62 +44,53 @@ void swap(t_link **first,t_link **second)
     (*second)->content = cup;
 }
 
-
-int main()
+void pushb(t_link **froma,t_link **tob)
 {
-    
-// -------------------------------------------------
-    t_link *node;
-    t_link *node2;
-    t_link *node3;
+    t_link *topa = *froma;
 
+    *froma = (*froma)->next;
+
+    (*froma)->prev = topa->prev;
+
+    topa->prev->next = *froma;
+
+    topa->next = *tob;
+    *tob = topa;
+    
+}
+
+void add_to_a(t_link **stack_a, int argc, char **argv) {
     int i = 1;
-    int i2 = 2;
-    int i3 = 3;
+    t_link *new;
+    t_link *old_node = NULL;
 
-
-    node = malloc(sizeof(t_link));
-    node->content = &i;
-    
-    node2 = malloc(sizeof(t_link));
-    node2->content = &i2;
-
-    node3 = malloc(sizeof(t_link));
-    node3->content = &i3;
-
-
-    node->next = node2;
-    node2->next = node3;
-    node3->next = node;
-
-    node->prev = node3;
-    node2->prev = node;
-    node3->prev = node2;
-
-// -------------------------------------------------
-    t_link *list;
-
-    list = malloc(sizeof(t_link));
-
-// -------------------------------------------------
-
-    swap(&node,&node2);
-
-
-
-// -------------------------------------------------
-
-    printf("node 1\n");
-    printf("%d\n",*(int *)node->content);
-    
-    printf("node 2\n");
-    printf("%d\n",*(int *)node2->content);
-
-    printf("node 3\n");
-    printf("%d\n",*(int *)node3->content);
-
-
-    free(node);
-    free(node2);
-    free(node3);
+    while (i < argc) 
+    {
+        int j = 0;
+        while (argv[i][j]) 
+        {
+            if (argv[i][j] < '0' || argv[i][j] > '9')
+                return; 
+            j++;
+        }
+        new = malloc(sizeof(t_link));
+        if (!new) return;
+        new->content = cpy(argv[i]);
+        if (*stack_a == NULL) 
+        {
+            *stack_a = new;
+            new->next = new;
+            new->prev = new;
+        } 
+        else 
+        {
+            old_node = *stack_a;
+            new->next = old_node;
+            new->prev = old_node->prev; 
+            old_node->prev->next = new;
+            old_node->prev = new;
+            *stack_a = new;
+        }
+        i++;
+    }
 }
