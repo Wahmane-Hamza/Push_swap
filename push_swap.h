282c02f9babe1e3594c4654e4ff725beae05e033
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-23 16:38:17 +0100

    df

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:16:44 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/22 19:54:11 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/23 14:41:06 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef struct s_link
 void push_a_b(t_link **stack_a,t_link **stack_b);
 void swap(t_link **first);
 void    add_to_a(t_link **stack_a,int argc, char **argv);
+void    fi_lst_or_lst_fi(t_link **stack,char type);
 
 void    *check_int(char *argv);
 t_link *create_cpy(char *str);
@@ -35,5 +36,7 @@ void    change_link(t_link **stack,t_link **new);
 void sa_sb(t_link **stack);
 void ss(t_link **stack_a,t_link **stack_b);
 void pa_pb(t_link **first,t_link **second);
+void    ra_rb_rra_rrb(t_link **stack,char type);
+void    rr_rrr(t_link **stack_a,t_link **stack_b,char type);
 
 #endif
