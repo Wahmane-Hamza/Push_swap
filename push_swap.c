d17583b4275875f1eb77425248605e3fbb372372
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-23 16:38:15 +0100

    df

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:16:21 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/22 19:54:05 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/23 16:35:38 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,37 @@
 
 void sa_sb(t_link **stack)
 {
+    if (!(*stack))
+        return ;
     swap(stack);
 }
 
 void ss(t_link **stack_a,t_link **stack_b)
 {
+    if (!(*stack_a) || !(*stack_b))
+        return ;
     swap(stack_a);
     swap(stack_b);
 }
 
 void pa_pb(t_link **first,t_link **second)
 {
+    if (!(*first) || !(*second))
+        return ;
     push_a_b(first,second);
 }
 
+void    ra_rb_rra_rrb(t_link **stack,char type)
+{
+    if (!(stack))
+        return ;
+    fi_lst_or_lst_fi(stack,type);
+}
+
+void    rr_rrr(t_link **stack_a,t_link **stack_b,char type)
+{
+    if (!(stack_a) || !(stack_b))
+        return ;
+    fi_lst_or_lst_fi(stack_a,type);
+    fi_lst_or_lst_fi(stack_b,type);
+}
