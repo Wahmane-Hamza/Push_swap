74e1205143a5d95d2739f18dd5c1f667b98e3880
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-23 16:38:09 +0100

    df

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 14:50:48 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/22 19:57:28 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/23 15:59:05 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,11 @@ int main(int argc, char **argv)
     pa_pb(&stack_a,&stack_b);
     pa_pb(&stack_a,&stack_b);
     pa_pb(&stack_a,&stack_b);
-    pa_pb(&stack_b,&stack_a);
 
-    ss(&stack_b,&stack_a);
-
+    // ss(&stack_b,&stack_a);
+    // ra_rb_rra_rrb(&stack_a,'l');
+    // ra_rb_rra_rrb(&stack_b,'l');
+    // rr_rrr(&stack_a,&stack_b,'l');
 // -------------------------------------------------
     printf("----STACK A----\n");
     t_link *break_a = stack_a;
@@ -44,7 +45,10 @@ int main(int argc, char **argv)
             printf(" %s |",(char *)stack_a->content);
             stack_a = stack_a->next;
             if(stack_a == break_a)
+            {
+                // printf(" %s |",(char *)stack_a->content);
                 break;
+            }   
         }
     }
     else
